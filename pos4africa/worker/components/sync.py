16a6fb6e66907410e958159246e8594dfa975e20
BB Jafar <jaffargee8@gmail.com>
2026-03-24 18:15:26 +0100

    Refactoring worker.components.sync.py

--- pos4africa/worker/components/sync.py
+++ pos4africa/worker/components/sync.py
@@ -2,15 +2,35 @@ from pos4africa.infra.supabase_client import spb_client
 from pos4africa.config import settings
 from pos4africa.shared.models.customer import Customer
 from pos4africa.shared.models.account import Account
-
+import socket, time
 
 class Sync:
       
+      _MAX_RETRIES = 3
+      _RETRY_DELAY = 1.5 # Seconds 1.5s (basic backoff)
+      
       @staticmethod
-      def _selectAll(table: str) -> list[dict] | None:
+      def _selectAll(table: str) -> list[dict]:
             if not table:
-                  print('Table must be provided.')
-                  return None
+                  raise ValueError("Table must be provided.")
+            
+            last_exc: Exception | None = None
+
+            for attemps in range(Sync._MAX_RETRIES):
+                  try:
+                        response = spb_client.table(table).select("*").execute()
+                        
+                        if not response or response.data is None:
+                              return []
+                        
+                        return response.data
+                  
+                  except (TimeoutError, socket.gaierror, ConnectionError) as e:
+                        last_exc = e
+                        time.sleep(Sync._RETRY_DELAY * (attemps + 1))
+                  
+                  except:
+                        pass
             
             return (spb_client.table(table).select('*').execute())
 
