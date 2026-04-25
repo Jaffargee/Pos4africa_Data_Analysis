fbc835ac77f0f81116c672353472f942aff5e02f
BB Jafar <jaffargee8@gmail.com>
2026-04-25 15:50:38 +0100

    Add OpenRouter API To GPT LLM for query

---
+++ excel_tracker.py
@@ -0,0 +1,19 @@
+
+
+EXCEL_FILE_BASE_URL = "https://fahadtahir.pos4africa.com/index.php/reports/generate/detailed_sales"
+
+ALL_TIME_REPORT = "?report_type=simple&report_date_range_simple=ALL_TIME&payment_type=&remove_store_account=1&show_tax=0&sale_type=all&currency=&register_id=&show_summary_only=0&email=&export_excel=1"
+LAST_30_DAYS_REPORT = "?report_type=simple&report_date_range_simple=LAST_30&payment_type=&remove_store_account=1&show_tax=0&sale_type=all&currency=&register_id=&show_summary_only=0&email=&export_excel=1"
+THIS_MONTH_REPORT = "?report_type=simple&report_date_range_simple=THIS_MONTH&payment_type=&remove_store_account=1&show_tax=0&sale_type=all&currency=&register_id=&show_summary_only=0&email=&export_excel=1"
+
+
+
+def write_file(data: bytes) -> None:
+      pass
+
+
+
+
+
+
+
--- tester.py
+++ tester.py
@@ -1,12 +1,19 @@
+import google.generativeai as genai
+import os, json
+from dotenv import load_dotenv
+
 from pos4africa.infra.supabase_client import spb_client
-import asyncio, json
 
-async def main():
-      
-      sale = (spb_client.table('sales').select('*, sale_items(*), payments(*)').eq('pos_sale_id', 300).execute())
-      
-      print(json.dumps(sale.data, indent=4))
-      
-      pass
+sales = json.dumps((spb_client.table('sales').select('*, sale_items(*), payments(*)').limit(100).execute()).data)
 
-asyncio.run(main())
+with open('data.txt', 'w') as out:
+      out.write(sales)
+
+load_dotenv()
+genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+
+model = genai.GenerativeModel('gemini-3-flash-preview')
+# response = model.generate_content("Explain quantum physics in one sentence.")
+response = model.generate_content(f"As a data analyst, Analyse this sales transaction and give an actionable intel on it \n {sales}")
+
+print(response.text)
